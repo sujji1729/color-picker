d7fbd6ddf8ec27a28196ad5af6694805f25536f3
sujji1729 <sujathanu96@gmail.com>
2024-08-01 19:37:39 +0530

    color picker

---
+++ colour.js
@@ -0,0 +1,27 @@
+let selectedColorHexCode = document.getElementById("selectedColorHexCode");
+let colorPickerContainer = document.getElementById("colorPickerContainer");
+
+
+function button1() {
+    colorPickerContainer.style.backgroundColor = "#e0e0e0";
+    selectedColorHexCode.textContent = "#e0e0e0";
+    selectedColorHexCode.style.color = "#49a6e9";
+}
+
+function button2() {
+    colorPickerContainer.style.backgroundColor = "#6fcf97";
+    selectedColorHexCode.textContent = "#6fcf97";
+    selectedColorHexCode.style.color = "#49a6e9";
+}
+
+function button3() {
+    colorPickerContainer.style.backgroundColor = "#56ccf2";
+    selectedColorHexCode.textContent = "#56ccf2";
+    selectedColorHexCode.style.color = "#49a6e9";
+}
+
+function button4() {
+    colorPickerContainer.style.backgroundColor = "#bb6bd9";
+    selectedColorHexCode.textContent = "#bb6bd9";
+    selectedColorHexCode.style.color = "#49a6e9";
+}
